50e4b8b7c943744858b20e224f02bcbc478799c7
Erik Brendel <erik.brendel@gmx.de>
2018-05-01 11:50:07 +0200

    WIP: Example testing

--- testing/test/test_examples.js
+++ testing/test/test_examples.js
@@ -17,6 +17,18 @@ describe('examples', () => {
         it(`Belegung "${example.name}" should${example.isValid ? '' : ' not'} be valid`,  () => {
             const belegungen = example.belegungen;
 
+            //TODO karma here!
+            localStorage.clear();
+            //TODO move all courses back to the pool
+            for (const belegung of belegungen) {
+                Semester.get(0).take(belegung);
+            }
+
+            const failingRules = ruleManager.checkAll();
+            const valid = failingRules.length === 0;
+            expect(valid).to.eq(example.isValid);
+
+
             expect('Not implemenet yet!').to.be.not.ok;
 
 
