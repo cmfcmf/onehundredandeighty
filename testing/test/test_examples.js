0ba0daece04a4c6456b15f48c03c9428ba0beafe
Erik Brendel <erik.brendel@gmx.de>
2018-05-03 23:35:46 +0200

    wdio browser testing

--- testing/test/test_examples.js
+++ testing/test/test_examples.js
@@ -1,6 +1,49 @@
+const path = require('path');
 const expect = require('chai').expect;
+const wdio = require('wdio');
+
+
+function getFileUrl(str) {
+    let pathName = path.resolve(str).replace(/\\/g, '/');
+    // Windows drive letter must be prefixed with a slash
+    if (pathName[0] !== "/") {
+        pathName = "/" + pathName;
+    }
+    return encodeURI("file://" + pathName);
+}
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 describe('examples', () => {
+
+    describe('wdio test', () => {
+        const browser = wdio.getBrowser({
+            desiredCapabilities: {
+                browserName: 'chrome',
+                chromeOptions: {
+                    args: ['headless', 'disable-gpu']
+                },
+            }
+        });
+
+        before(wdio.initSelenium);
+
+        before(wdio.wrap(function() {
+            browser.init();
+            browser.url(getFileUrl("../index.html"));
+        }));
+
+        after(wdio.wrap(function() {
+            browser.end();
+        }));
+
+        it('Should return "onehundredandeighty" when asked about page title', wdio.wrap(function () {
+            expect(browser.getTitle()).to.eq('onehundredandeighty');
+        }));
+    }).timeout(60000);
+
 
     const examples = [];
     function addExample(name, belegungen, isValid = true) {
@@ -12,7 +55,7 @@ describe('examples', () => {
     }
     require('./example_list.js')(addExample);
 
-    for(let i = 0; i < examples.length; i++) {
+    for(let i = 0; i < 0/*examples.length*/; i++) {
         const example = examples[i];
         it(`Belegung "${example.name}" should${example.isValid ? '' : ' not'} be valid`,  () => {
             const belegungen = example.belegungen;
